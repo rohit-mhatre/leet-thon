f7a00dc3e1a48c929ab89c14878578f490c197af
Rohit Mhatre <101646263+rohit-mhatre@users.noreply.github.com>
2024-07-09 08:48:26 +0000

    removed everything

--- 125-valid-palindrome/valid-palindrome.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        temp = ''
-        for ch in s:
-            if ch.isalpha() or ch.isdigit():
-                temp = temp + ch.lower()
-        return (temp == temp[::-1])
