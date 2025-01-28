39d7a973e50e7c6b44ff9f436a7efa957a85e08d
Rohit Mhatre <101646263+rohit-mhatre@users.noreply.github.com>
2025-01-28 21:46:55 +0000

    reset repo

--- neetcode150/1-two-sum/two-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #create a dictionary
-        map = {}
-        for i, n in enumerate(nums):
-            temp = target - n
-            if temp in map:
-                return [map[temp], i]
-            map[n] = i
